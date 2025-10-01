14c4d10f9750e827b9d714352ce566090e3fdef4
LucasViana130 <lucasgviana1@gmail.com>
2025-10-01 15:45:06 -0300

    Feat: criar componente footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#23C8AA] text-white p-4 mt-auto">
+      <div className="flex justify-center">
+        <button 
+          className="w-12 h-12  bg-opacity-20 flex items-center justify-center"
+          aria-label="Ícone de acessibilidade"
+        >
+        </button>
+      </div>
+    </footer>
+  );
+}
