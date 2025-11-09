a0440cf670a08925f183c0f14c5f22aad5d5fa9d
deryksouza2006 <deryksouza2006@gmail.com>
2025-11-09 12:54:04 -0300

    feat: config Global

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './accessibility.css';
 import App from './App.tsx';
+import { AccessibilityProvider } from './context/AccessibilityContext';
+import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AccessibilityProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </AccessibilityProvider>
   </StrictMode>,
 )
